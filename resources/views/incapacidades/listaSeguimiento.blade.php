5d89b57956db10486ae2aecd655514831cfc3e0b
Sergio Andres Carrillo <desarrollo3@copetran.com>
2025-06-03 16:17:26 -0500

    botones de volver

--- resources/views/incapacidades/listaSeguimiento.blade.php
+++ resources/views/incapacidades/listaSeguimiento.blade.php
@@ -15,6 +15,7 @@
 <div class="container-fluid p-0 border rounded" style="min-height:150px; background-color: white">
     <div class="border rounded-top d-flex justify-content-between align-items-center px-4 bg-secondary">
         <span class="text-left text-light fs-5 fw-medium py-1">Seguimiento incapacidades</span>
+        <a class="btn fw-bold my-2 bg-primary" onclick="window.history.back()">Volver</a>
     </div>
 
     <div id="no-more-tables" class="row p-4">
@@ -32,8 +33,7 @@
             data-detail-formatter="detalleIncapacidad"
             data-mobile-responsive="true"
             data-check-on-init="true"
-            data-url="{{ route('gestion-incapacidades.seguimiento.cargarDatos') }}"        
-            data-pagination-parts="['pageSize', 'pageList', 'pageNext', 'pagePrev']">   
+            data-url="{{ route('gestion-incapacidades.seguimiento.cargarDatos') }}">   
             <thead class="table-primary">
                 <tr>
                     <th data-field="IncPerNom">Nombre</th>
--- resources/views/submodulos/listaSubModulos.blade.php
+++ resources/views/submodulos/listaSubModulos.blade.php
@@ -16,6 +16,7 @@
 
     <div class="border rounded-top d-flex justify-content-between align-items-center px-4 bg-secondary">
         <span class="text-left text-light fs-5 fw-medium py-1">SubMódulos</span>
+        <a class="btn fw-bold my-2 bg-primary" onclick="window.history.back()">Volver</a>
     </div>
 
     @permite('configuracion.gestion_sistema.crear')
