74f000519b830729d4e246be2bf1c23a9794f6ae
Sergio Andres Carrillo <desarrollo3@copetran.com>
2025-07-03 09:16:21 -0500

    cambios init

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,27 +32,24 @@ $.fn.modal = function (...args) {
 
 // SweetAlert2
 import Swal from 'sweetalert2';
-window.Swal = Swal;
+window.Swal = Swal; 
 
 // XLSX (excels)
 import * as XLSX from 'xlsx';
 window.XLSX = XLSX;
 
-import { deshabilitarSubmit,
-        habilitarSubmit,
+import { deshabilitarSubmit, 
+        habilitarSubmit, 
         actualizarReloj,
-        handleMenuActive,
+        handleMenuActive, 
         abrirArchivo,
         exportarExcel,
         actualizarEstado,
         mostrarToast,
-        createThemeManager
+        darkModeEnable
     } from './utils';
 
 import { initTablaBootstrapTable, generarDetalle } from './tablasAjusteColumnas';
-
-const themeManager = createThemeManager();
-window.themeManager = themeManager;
 
 window.deshabilitarSubmit = deshabilitarSubmit;
 window.habilitarSubmit = habilitarSubmit;
@@ -60,6 +57,8 @@ window.abrirArchivo = abrirArchivo;
 window.exportarExcel = exportarExcel;
 window.actualizarEstado = actualizarEstado;
 window.mostrarToast = mostrarToast;
+window.darkModeEnable = darkModeEnable;
+
 window.initTablaBootstrapTable = initTablaBootstrapTable;
 window.generarDetalle = generarDetalle;
 
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -1,180 +1,158 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-   <meta charset="utf-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1" />
-   <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-   <title>@yield('title')</title>
+    <title>@yield('title')</title>
 
-   <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png" />
-   <link rel="preload" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css" as="style"
-      onload="this.onload=null;this.rel='stylesheet'" />
-   <noscript>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css" />
-   </noscript>
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    </noscript>
 
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-   <link rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-duallistbox/dist/bootstrap-duallistbox.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-duallistbox/dist/bootstrap-duallistbox.min.css">
 
-   <script>
-      // Dark Mode Toggle
-      document.addEventListener("DOMContentLoaded", () => {
+    <style>
+        body {
+            visibility: hidden;
+        }
+        body.show {
+            visibility: visible;
+        }
+    </style>
 
-      });
-   </script>
-
-   <style>
-      body {
-         visibility: hidden;
-      }
-
-      body.show {
-         visibility: visible;
-      }
-   </style>
-
-   <!--Vite -->
-   @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/custom.css', 'resources/css/mobile.css'])
+     <!--Vite -->
+     @vite(['resources/css/app.css', 'resources/js/app.js','resources/css/custom.css','resources/css/mobile.css','resources/css/darkmode.css'])
 </head>
 
 <body class="sidebar-mini sidebar-collapse layout-fixed">
-   <!-- Toast componente (para AJAX) -->
-   <div class="toast-container position-fixed top-0 end-0 p-3" id="toastContainer"></div>
+    <!-- Toast componente (para AJAX) -->
+    <div class="toast-container position-fixed top-0 end-0 p-3" id="toastContainer"></div>
 
-   <div class="wrapper">
-      <!-- Pantalla de carga -->
-      <div class="preloader flex-column justify-content-center align-items-center">
-         <div class="spinner-border text-secondary mt-2" role="status"></div>
-         <p class="mt-1 fw-medium text-muted">Cargando...</p>
-      </div>
+    <div class="wrapper">
+        <!-- Pantalla de carga -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <div class="spinner-border text-secondary mt-2" role="status"></div>
+            <p class="mt-1 fw-medium text-muted">Cargando...</p>
+        </div>
+        
+        <!-- Sidebar -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4 text-dark bg-light">
+            <div class="sidebar">
+            <!-- Usuario -->
+                <div class="user-panel mt-3 pt-1 pb-0 d-flex flex-column" style=" border:none">
 
-      <!-- Sidebar -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-         <div class="sidebar">
-            {{-- Usuario --}}
-            <div class="user-panel mt-3 pt-1 pb-0 d-flex flex-column" style="border: none">
-               {{-- Icono cambiar theme --}}
-               <button class="btn rounded-circle me-3" title="Cambiar modo oscuro" style="width: 50px"
-                  data-theme-toggle>
-                  <i class="light-icon"></i>
-               </button>
+                    <div style="width: 1.5em; display: flex; align-items: center; justify-content: flex-start;" class="ms-3">
+                        <i id="toggleDarkMode" class="bi bi-sun text-dark" style="cursor: pointer;"></i>
+                    </div>
+                
+                    <div class="info d-flex flex-column align-items-center" style="width: 235px; margin: 0 auto; user-select: none;">
+                        <img src="{{ asset('img/LogoCope.png') }}" class="logoCope" style="width:100%; height: auto;" alt="Logo Cope">
+                        <img src="{{ asset('img/LogoCopeBlancoFull.png') }}" class="logoCopeBlanco py-3" style="width:80%; height: auto; display:none" alt="Logo Cope ">
+                    </div>
 
-               <div class="info d-flex flex-column align-items-center"
-                  style="width: 235px; margin: 0 auto; user-select: none">
-                  <img src="{{ asset('img/LogoCope.png') }}" class="logoCope" style="width: 100%; height: auto"
-                     alt="Logo Cope" />
-                  <img src="{{ asset('img/LogoCopeBlancoFull.png') }}" class="logoCopeBlanco py-3"
-                     style="width: 80%; height: auto; display: none" alt="Logo Cope " />
-               </div>
+                    <div class="info" style="user-select: none; font-size: 0.75em;">
+                        <hr class="mb-2 text-muted">
+                        <span class="fw-bold text-dark">Nombre:</span>
+                        <span class="fw-medium">
+                            {{ $user->persona?->PerNombres }} {{ $user->persona?->PerApellidos }}
+                        </span>
+                        <br>
 
-               <div class="info" style="user-select: none; font-size: 0.75em">
-                  <hr class="mb-2 text-muted" />
-                  <span class="fw-bold">Nombre:</span>
-                  <span class="fw-medium">
-                     {{ $user->persona?->PerNombres }}
-                     {{ $user->persona?->PerApellidos }}
-                  </span>
-                  <br />
+                        <span class="fw-bold text-dark"> Rol:</span>
+                        <span class="fw-medium">
+                            {{ $user->rol }}
+                        </span>
+                        <br>
 
-                  <span class="fw-bold"> Rol:</span>
-                  <span class="fw-medium">
-                     {{ $user->rol }}
-                  </span>
-                  <br />
+                        <span class="fw-bold text-dark"> Ultima sesión:</span>
+                        <span class="fw-medium">
+                            {{ $user->ultimaSesion->first()->SesionFechReg }} 
+                            {{ $user->ultimaSesion->first()->SesionHorReg }} 
+                        </span>
+                    </div>
 
-                  <span class="fw-bold"> Ultima sesión:</span>
-                  <span class="fw-medium">
-                     {{ $user->ultimaSesion->first()->SesionFechReg }}
-                     {{ $user->ultimaSesion->first()->SesionHorReg }}
-                  </span>
-               </div>
+                    <div class="horaComponent info text-secondary">
+                        <span id="horas"></span>:<span id="minutos"></span>:<span id="segundos"></span>&nbsp;<span id="ampm"></span>
+                        <hr class="mb-0 mt-2 text-muted">
+                    </div>   
+                </div>
 
-               <div class="horaComponent info text-muted">
-                  <span id="horas"></span>:<span id="minutos"></span>:<span id="segundos"></span>&nbsp;<span
-                     id="ampm"></span>
-                  <hr class="mb-0 mt-2 text-muted" />
-               </div>
+                <!-- Menú de Navegación -->
+                @include('menu')
             </div>
+        </aside>
 
-            {{-- Menú de Navegación --}}
-            @include('menu')
-         </div>
-      </aside>
+        <!-- Contenido Principal -->
+        <div class="content-wrapper">
+            <section class="content-header mb-4 p-2 py-1 d-flex justify-content-between align-items-center bg-secondary"
+                style="position: sticky; top: 0; z-index: 1030;">
+                <div class="container-fluid my-2">
+                    <a class="pushmenu btn btn-sm text-light" 
+                        data-widget="pushmenu" 
+                        data-enable-remember="true" 
+                        href="#" 
+                        role="button">
+                        <i class="fas fa-bars"></i>
+                    </a>    
+                    <img src="{{ asset('img/LogoCopeBlanco.png') }}" style="width:70px; height: 15px;" alt="Logo Cope" class="d-md-none">
+                </div>
+            </section>
 
-      <!-- Contenido Principal -->
-      <div class="content-wrapper">
-         <section class="content-header mb-4 p-2 py-1 d-flex justify-content-between align-items-center bg-primary"
-            style="position: sticky; top: 0; z-index: 1030">
-            <div class="container-fluid my-2">
-               <a class="pushmenu btn btn-sm text-light" data-widget="pushmenu" data-enable-remember="true"
-                  href="#" role="button">
-                  <i class="fas fa-bars"></i>
-               </a>
-               <img src="{{ asset('img/LogoCopeBlanco.png') }}" style="width: 70px; height: 15px" alt="Logo Cope"
-                  class="d-md-none" />
-            </div>
-         </section>
+            <section class="content">
+                <div class="container-fluid mb-4">
+                    @yield('breadcrumb')
+                    @yield('content')
+                </div>
+            </section>
+        </div>
+    </div>
 
-         <section class="content">
-            <div class="container-fluid mb-4">
-               @yield('breadcrumb')
-               @yield('content')
-            </div>
-         </section>
-      </div>
-   </div>
+    <!-- Renderizado de componentes-->
+    <x-alert /> 
+    <x-modal />
+    <x-toast />
+    <x-loader />
+    
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-duallistbox/dist/jquery.bootstrap-duallistbox.min.js" defer></script>
+    
+    <script>
+        //Modal (evita errores de focus)
+        document.addEventListener('hidden.bs.modal', function (event) {
+            if (document.activeElement) {
+                document.activeElement.blur();
+            }
+        });
+        
+        // Función para mostrar el toast
+        window.addEventListener('DOMContentLoaded', function () {
+            const title = sessionStorage.getItem('toastTitle');
+            const type = sessionStorage.getItem('toastType');
 
-   <!-- Renderizado de componentes-->
-   <x-alert />
-   <x-modal />
-   <x-toast />
-   <x-loader />
+            if (title && type) {
+                mostrarToast(title, type); 
+                sessionStorage.removeItem('toastTitle');
+                sessionStorage.removeItem('toastType');
+            }
+        });
 
-   <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js" defer></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap-duallistbox/dist/jquery.bootstrap-duallistbox.min.js" defer>
-   </script>
+        //dark mode toggle 
+        document.addEventListener('DOMContentLoaded', () => {
+            darkModeEnable()
+        });    
 
-   <script>
-      //Modal (evita errores de focus)
-      document.addEventListener("hidden.bs.modal", function(event) {
-         if (document.activeElement) {
-            document.activeElement.blur();
-         }
-      });
+        // Mostrar el cuerpo del documento después de cargar todo
+        window.addEventListener('load', function () {
+            document.body.classList.add('show');
+        });
 
-      // Función para mostrar el toast
-      window.addEventListener("DOMContentLoaded", function() {
-         const title = sessionStorage.getItem("toastTitle");
-         const type = sessionStorage.getItem("toastType");
+    </script>
 
-         if (title && type) {
-            mostrarToast(title, type);
-            sessionStorage.removeItem("toastTitle");
-            sessionStorage.removeItem("toastType");
-         }
-
-         themeManager.initTheme();
-         // Configurar listener para cambios del sistema
-         themeManager.setupSystemThemeListener();
-         // Configurar botón toggle si existe
-         const toggleButton = document.querySelector("[data-theme-toggle]");
-         if (toggleButton) {
-            toggleButton.addEventListener("click", () => {
-               themeManager.toggleTheme();
-            });
-         }
-      });
-
-      // Mostrar el cuerpo del documento después de cargar todo
-      window.addEventListener("load", function() {
-         document.body.classList.add("show");
-      });
-   </script>
-
-   @stack('script')
+    @stack('script')
 </body>
-
 </html>
