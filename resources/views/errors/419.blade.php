ccaf4b083876bfc79494f59eaed2183c4167de5b
Sergio Andrés Carrillo Muñoz <checho530@gmail.com>
2025-07-28 09:55:36 -0500

    control sesion expirada

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <script>
+        window.location.href = "{{ route('login.form', ['expired' => 1]) }}";
+    </script>
+</head>
+<body></body>
+</html>
