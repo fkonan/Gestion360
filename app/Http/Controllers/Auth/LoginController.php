cf733b80ab8615257fd9a76295dde1388da8cb43
fhernandez <fabian.hernandez.murcia@gmail.com>
2025-08-01 10:04:55 -0500

    Ajustes conexion logtrans-autogestion

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Models\GESTIONADMIN\Persona;
 use App\Models\LOGTRANS\PerPersonas;
 use App\Services\Auth\LoginValidatorService;
 use App\Services\Auth\RegistroSesionService;
+use App\Services\EmpleadoService;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +18,7 @@ class LoginController extends Controller
 {
    public function showLoginForm(Request $request)
    {
-      $darkMode = $request->cookie('darkMode') === 'enabled';
-      return view('auth.login', compact('darkMode'));
+      return view('auth.login');
    }
 
    public function login(Request $request)
@@ -36,56 +36,28 @@ class LoginController extends Controller
 
 
       try {
-         // Buscar usuario relacionado a la persona
+         $validador = new LoginValidatorService();
+         // Buscar usuario relacionado a la persona en autogestion
          $persona = Persona::with(['usuario'])
             ->where('PerNumDoc', $request->identificacion)
             ->first();
-
          $user = $persona?->usuario;
-
-
-
-
-
-         $per_persona = PerPersonas::where('IDENTIFICACION', $request->identificacion)->first();
-         if ($per_persona && $this->checkSHA1Password($request->password, $per_persona->clave)) {
-
-
-            if ($this->shouldRehashPassword($user)) {
-               $user->update(['Password' => Hash::make($request->password)]);
+         if (!$user) {
+            // Buscar la persona en logtrans, valido que sea empleado y este activo
+            $user = $validador->validarLogtrans($request->identificacion, $request->password);
+         } else {
+            // Validar credenciales autogestion
+            if (!password_verify($request->password, $user->Password)) {
+               return back()->withInput()->withErrors(['identificacion' => 'Identificación o contraseña incorrectos']);
             }
-
-            Auth::login($user);
-
-            // Opcional: Actualizar a bcrypt para mayor seguridad
-            if ($this->shouldRehashPassword($user)) {
-               $user->update(['clave' => Hash::make($request->password)]);
+            // Validaciones adicionales (empleado activo, estados, etc.)
+            $resultado = $validador->validar($user, $request->identificacion);
+            if ($resultado) {
+               return toast($resultado['message'], $resultado['type']);
             }
-
-            return redirect()->intended('/dashboard');
          }
-         dd('error de clave');
-
-
-
-
-
-         // Validar credenciales
-         if (!$user || !password_verify($request->password, $user->Password)) {
-            return back()->withInput()->withErrors(['identificacion' => 'Identificación o contraseña incorrectos']);
-         }
-
-         // Validaciones adicionales (empleado activo, estados, etc.)
-         $validador = new LoginValidatorService();
-         $resultado = $validador->validar($user, $request->identificacion);
-
-         if ($resultado) {
-            return toast($resultado['message'], $resultado['type']);
-         }
-
          // Registrar evento de login
          RegistroSesionService::registrar('LOGIN', $user->IdUsuario);
-
          // Iniciar sesión
          Auth::login($user);
          return redirect()->intended(route('home'));
@@ -108,19 +80,5 @@ class LoginController extends Controller
       }
    }
 
-   private function checkSHA1Password($plainPassword, $hashedPassword)
-   {
-      // Replicar exactamente el proceso de Java
-      $isoString = mb_convert_encoding($plainPassword, 'ISO-8859-1', 'UTF-8');
-      $sha1Hash = sha1($isoString);
 
-      return hash_equals($hashedPassword, $sha1Hash);
-   }
-
-   private function shouldRehashPassword($user)
-   {
-      // Opcional: migrar gradualmente a bcrypt
-      return Hash::needsRehash($user->Password);
-      //return false; // Por ahora mantener SHA-1
-   }
 }
--- app/Services/EmpleadoService.php
+++ app/Services/EmpleadoService.php
@@ -7,48 +7,55 @@ use Illuminate\Support\Facades\Cache;
 
 class EmpleadoService
 {
-    //empleado activo en LOGTRANS
-    public static function esEmpleadoActivo($identificacion): bool
-    {
-        return DB::connection('oracle')
+   //empleado activo en LOGTRANS
+   public static function esEmpleadoActivo($identificacion,$retornarPersona=false): bool|object
+   {
+      $query = DB::connection('oracle')
+         ->table('per_empresapersonas as ep')
+         ->join('per_personas as p', 'ep.pe_id_pe', '=', 'p.id')
+         ->where('ep.activo', 1)
+         ->where('ep.estborrado', 0)
+         ->where('ep.tp_id', 1)
+         ->where('p.identificacion', $identificacion);
+
+      if ($retornarPersona) {
+         return $query->first(); // Devuelve el primer registro encontrado
+      }
+
+      return $query->exists(); // Solo valida existencia
+   }
+
+   //Obtener documentos válidos desde Oracle (solo empleados activos) y se cachean por 5 minutos
+   public static function documentosEmpleadosValidos(): array
+   {
+      return Cache::remember('empleados_oracle', 300, function () {
+         return DB::connection('oracle')
             ->table('per_empresapersonas as ep')
             ->join('per_personas as p', 'ep.pe_id_pe', '=', 'p.id')
             ->where('ep.activo', 1)
             ->where('ep.estborrado', 0)
             ->where('ep.tp_id', 1)
-            ->where('p.identificacion', $identificacion)
-            ->exists();
-    }
-
-    //Obtener documentos válidos desde Oracle (solo empleados activos) y se cachean por 5 minutos
-    public static  function documentosEmpleadosValidos(): array{
-        return Cache::remember('empleados_oracle', 300, function () {
-            return DB::connection('oracle')
-                ->table('per_empresapersonas as ep')
-                ->join('per_personas as p', 'ep.pe_id_pe', '=', 'p.id')
-                ->where('ep.activo', 1)
-                ->where('ep.estborrado', 0)
-                ->where('ep.tp_id', 1)
-                ->pluck('p.identificacion')
-                ->toArray();
-        });
-    }
-
-    //lista de centros de costo para un grupo de identificaciones
-    public static function obtenerCentrosCostoMasivos(array $identificaciones): array{
-        return DB::connection('oracle')
-            ->table('per_personas as p')
-            ->join('per_empresapersonas as ep', 'p.id', '=', 'ep.pe_id_pe')
-            ->join('per_cargoccostos as cc', 'ep.cc_id', '=', 'cc.id')
-            ->join('per_centrocostos as ct', 'cc.ct_codigo', '=', 'ct.codigo')
-            ->whereIn('p.identificacion', $identificaciones)
-            ->where('ep.activo', 1)
-            ->where('ep.estborrado', 0)
-            ->where('cc.activo', 1)
-            ->where('cc.estborrado', 0)
-            ->where('ct.estado', 1)
-            ->where('ct.estborrado', 0)
-            ->pluck('ct.descripcion', 'p.identificacion') 
+            ->pluck('p.identificacion')
             ->toArray();
-    }
+      });
+   }
+
+   //lista de centros de costo para un grupo de identificaciones
+   public static function obtenerCentrosCostoMasivos(array $identificaciones): array
+   {
+      return DB::connection('oracle')
+         ->table('per_personas as p')
+         ->join('per_empresapersonas as ep', 'p.id', '=', 'ep.pe_id_pe')
+         ->join('per_cargoccostos as cc', 'ep.cc_id', '=', 'cc.id')
+         ->join('per_centrocostos as ct', 'cc.ct_codigo', '=', 'ct.codigo')
+         ->whereIn('p.identificacion', $identificaciones)
+         ->where('ep.activo', 1)
+         ->where('ep.estborrado', 0)
+         ->where('cc.activo', 1)
+         ->where('cc.estborrado', 0)
+         ->where('ct.estado', 1)
+         ->where('ct.estborrado', 0)
+         ->pluck('ct.descripcion', 'p.identificacion')
+         ->toArray();
+   }
 }
